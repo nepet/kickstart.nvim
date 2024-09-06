c7ced5aef0eb2e728c0bd18686b5b4f180cc2025
Peter Neuroth <pet.v.ne@gmail.com>
2024-09-06 16:37:25 +0200

    Add Neogit
    Signed-off-by: Peter Neuroth <pet.v.ne@gmail.com>

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,32 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'akinsho/bufferline.nvim',
+    version = '*',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    options = {
+      diagnostics = 'nvim_lsp',
+      offsets = {
+        {
+          filetype = 'NvimTree',
+          text = 'File Explorer',
+          text_align = 'left',
+          separator = true,
+        },
+      },
+    },
+  },
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
+
+      -- Only one of these is needed, not both.
+      'nvim-telescope/telescope.nvim', -- optional
+    },
+    config = true,
+  },
+}
